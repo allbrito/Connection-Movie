3734fd9d41d8e51606548d4b6587af50d6a92575
Allan Brito <allbdesenvolvedor04@gmail.com>
2026-05-13 16:43:42 -0300

    Adicionando testes Junit na classe FilmeTest

--- src/br/ucsal/connection_movie/test/FilmeTest.java
+++ src/br/ucsal/connection_movie/test/FilmeTest.java
@@ -1,4 +1,60 @@
 package br.ucsal.connection_movie.test;
 
+import br.ucsal.connection_movie.model.Filme;
+import br.ucsal.connection_movie.model.PerfilCinefilo;
+import br.ucsal.connection_movie.model.enums.ClassificacaoEtaria;
+import br.ucsal.connection_movie.model.enums.Genero;
+import br.ucsal.connection_movie.model.enums.Idioma;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FilmeTest {
+
+    Filme filme;
+    List<Idioma> idiomas;
+    List<Genero> generos;
+
+    PerfilCinefilo perfilCinefilo;
+    @BeforeEach
+    void prepararAmbiente() {
+
+        idiomas = new ArrayList<Idioma>();
+        idiomas.add(Idioma.PORTUGUES);
+        perfilCinefilo = new PerfilCinefilo(2, 5, ClassificacaoEtaria.DEZ, idiomas);
+        generos = new ArrayList();
+        generos.add(Genero.ACAO);
+
+        filme = new Filme("F01", "Duna: Parte Dois", 3, generos, ClassificacaoEtaria.DEZ, Idioma.PORTUGUES, 78.0);
+    }
+
+    @Test
+    void testCriacaoDeFilmeComTodosOsAtributos() {
+        assertAll("Testa se em um filme todas os atributos estão preenchidos",
+                () -> assertTrue(filme.getId()!=null),
+                () -> assertTrue(filme.getNome() != null),
+                () -> assertTrue(filme.getDuracao() > 0),
+                () -> assertTrue(filme.getGeneros() != null),
+                () -> assertTrue(!filme.getGeneros().isEmpty()),
+                () -> assertTrue(filme.getClassificacaoEtaria()!=null),
+                () -> assertTrue(filme.getIdioma()!=null),
+                () -> assertTrue(filme.getPopularidade() >= 0)
+        );
+    }
+
+
+    @Test
+    @DisplayName("Testa se o sistema reconhece a duplicidade de um filme pelo seu ID")
+    void testFilmesIguais() {
+
+        Filme filme2 = new Filme("F01", "Duna: Parte Dois", 3, generos, ClassificacaoEtaria.DEZ, Idioma.PORTUGUES, 78.0);
+
+        assertTrue(filme.equals(filme2));
+    }
 }
